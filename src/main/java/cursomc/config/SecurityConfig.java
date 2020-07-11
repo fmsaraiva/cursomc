4ebe2aaebbb703d0b56448345183dc5340c26a0e
fmsaraiva <fmsaraiva@gmail.com>
2020-07-11 19:22:39 -0300

    Configuracao inicial do spring security

---
+++ src/main/java/cursomc/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package cursomc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	private static final String[] PUBLIC_MATTERS = {
+			"/h2-console/**"
+	};
+	
+	private static final String[] PUBLIC_MATTERS_GET = {
+			"/produtos/**",
+			"/categorias/**"
+	};
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception{
+		http.cors().and().csrf().disable();
+		
+		http.authorizeRequests()
+		    .antMatchers(HttpMethod.GET, PUBLIC_MATTERS_GET).permitAll()
+		    .antMatchers(PUBLIC_MATTERS).permitAll()
+		    .anyRequest().authenticated();
+		
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //Para assegurar que o backend não vai guardar sessão de usuário
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
